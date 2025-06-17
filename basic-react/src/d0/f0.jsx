d44c2c48fe7c867610bd374b1c9ebd4d4f1019b9
CPunisher <1343316114@qq.com>
2025-06-17 16:26:09 +0800

    chore: 1000 -> basic react

---
+++ basic-react/src/d0/f0.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import I from "@iconify-icons/material-symbols/auto-stories-rounded.js";
+import { Icon } from "@iconify/react/dist/offline";
+
+function Component() {
+  return <div className="">Hello</div>;
+}
+
+export default Component;
---
+++ basic-react/src/f0.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import I from "@iconify-icons/material-symbols/phone-android-sharp.js";
+import { Icon } from "@iconify/react/dist/offline";
+import C0 from "./d0/f0.jsx";
+
+function Component() {
+  return (
+    <div className="">
+      <C0 />
+    </div>
+  );
+}
+
+export default Component;
