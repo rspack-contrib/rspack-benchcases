c5125f11ef88099f5a78510e4a0d7ecc01a3b168
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-07-19 22:09:22 +0800

    fix: alias `@swc/helpers` in threejs

--- threejs/rspack.config.js
+++ threejs/rspack.config.js
@@ -1,4 +1,9 @@
 /** @type {import("@rspack/cli").Configuration} */
 module.exports = {
-	entry: { main: "./src/Three.js" }
+  entry: { main: "./src/Three.js" },
+  resolve: {
+    alias: {
+      "@swc/helpers": require.resolve("@swc/helpers"),
+    },
+  },
 };
